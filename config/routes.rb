0170cd24155310d37f9d45b2330d4bf83f6dd440
Steve <vduong.steven@gmail.com>
2024-09-20 19:58:44 -0700

    First look of homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get "home/index"
+
+  root "home#index" # Sets this as the home default page so no need for home/index path in URL
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
